086436dd8feea257e95c08915d097ff7ad26d253
phuwaditever <phuwaditever@gmail.com>
2018-02-11 18:50:52 +0700

    part 1 submit

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import React, { Component } from 'react';
 import { View, Text, TextInput, Button } from 'react-native';
+import Validator from 'validator';
 import LoginPage from '../pages/LoginPage';
 
 class LoginForm extends Component{ 
@@ -8,13 +9,28 @@ class LoginForm extends Component{
         super(props);
         this.state = {
             email: '',
-            password: ''
+            password: '',
+            errors: {}
         };
 
         this.onSubmit = this.onSubmit.bind(this);
+        this.checkError = this.checkError.bind(this);
         //this.onChange = this.onChange.bind(this);
     }
-
+    checkPassword(){
+        console.log('checkPassword',this.state.password);
+        if(!Validator.isPassword(this.state.password))
+            this.setState({errors: {password:'not'}})
+        else
+            this.setState({errors: {password:''}});
+    }
+    checkError(){
+        console.log('checkEmail',this.state.email);
+        if(!Validator.isEmail(this.state.email))
+            this.setState({errors: {email:'not email'}})
+        else
+            this.setState({errors: {email:''}});
+    }
     onSubmit(){
         console.log('onSubmit',this.state);
     }
@@ -27,12 +43,16 @@ class LoginForm extends Component{
                     placeholder="email"
                     onChangeText={(email) => this.setState({ email })}
                     value={this.state.email}
+                    onBlur={this.checkEmail}
                 />
+                <Text>{this.state.errors.email}</Text>
                 <TextInput 
                     style={{backgroundColor:'pink',marginTop:10,marginBottom:10}}
                     secureTextEntry = 'true'
                     placeholder="password"
+                    onBlur={this.checkPassword}
                 />
+                <Text>{this.state.errors.password}</Text>
             </View>
         );
     }
