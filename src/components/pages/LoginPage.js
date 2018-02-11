dc41a34ed0ea406203bbecd767bed76e5dac2c77
phuwaditever <phuwaditever@gmail.com>
2018-02-11 18:57:30 +0700

    update login page

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -5,9 +5,14 @@ import LoginForm from '../forms/LoginForm';
 
 // const LoginPage = () => <Text>Login Page</Text>;
 class LoginPage extends Component {
+    submit(){
+        console.log('Submit on LoginForm');
+    }
     render() {
         return (
-            <LoginForm />
+            <LoginForm 
+            submit = {this.submit}
+            />
         );
     }
 }
