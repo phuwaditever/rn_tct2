829c1e794dcd50d5c4b8fa7b5a30a9b981d639d8
phuwaditever <phuwaditever@gmail.com>
2018-02-11 17:11:07 +0700

    part 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 console.log('app component is working.');
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import { Text,View } from 'react-native';
 import Navbar from './component/Navbar';
 import LoginPage from './components/pages/LoginPage';
 import LoginForm from './components/forms/LoginForm';
-=======
-import { Text, View } from 'react-native';
-import Navbar from './components/Navbar';
-import LoginPage from './components/pages/LoginPage';
->>>>>>> 174fdf3ca7b3e4b3342b7294a102a058a0f23a69
 
 export default class App extends Component {
   componentDidMount() {
@@ -21,17 +15,10 @@ export default class App extends Component {
   render() {
     console.log('render()');
     return (
-<<<<<<< HEAD
       <View>
         <Navbar/>
         <LoginPage/>
       </View>
-=======
-        <View>
-          <Navbar/>
-          <LoginPage />
-        </View>
->>>>>>> 174fdf3ca7b3e4b3342b7294a102a058a0f23a69
       );
     }
 }
--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import { View, Text, TextInput, Button } from 'react-native';
 import LoginPage from '../pages/LoginPage';
@@ -7,24 +6,12 @@ import LoginPage from '../pages/LoginPage';
 class LoginForm extends Component{ 
     constructor(props){
         super(props);
-=======
-import { 
-    View, Text,
-    TextInput, Button
-} from 'react-native';
-
-class LoginForm extends React.Component {
-    constructor(props) {
-        super(props);
-
->>>>>>> 174fdf3ca7b3e4b3342b7294a102a058a0f23a69
         this.state = {
             email: '',
             password: ''
         };
 
         this.onSubmit = this.onSubmit.bind(this);
-<<<<<<< HEAD
         //this.onChange = this.onChange.bind(this);
     }
 
@@ -45,43 +32,10 @@ class LoginForm extends React.Component {
                     style={{backgroundColor:'pink',marginTop:10,marginBottom:10}}
                     secureTextEntry = 'true'
                     placeholder="password"
-=======
-    }
-
-    onSubmit() {
-        console.log('onSubmit', this.state);
-    }
-    
-    render() {
-        return (
-            <View style={{ padding: 10 }} >
-                <Text>Login</Text>
-                <TextInput
-                    style={{ padding: 5, height: 40, borderColor: 'orange', borderWidth: 1}}
-                    onChangeText={(email) => this.setState({ email })}
-                    value={this.state.email}
-                    placeholder="prawee@hotmail.com"
-                />
-
-                <TextInput 
-                    style={{padding: 5, marginTop: 5, height: 40, borderColor: 'orange', borderWidth: 1}}
-                    placeholder="Password"
-                    secureTextEntry
->>>>>>> 174fdf3ca7b3e4b3342b7294a102a058a0f23a69
-                    onChangeText={(password) => this.setState({ password })}
-                    value={this.state.password}
-                />
-                <Button 
-                    title="Login"
-                    onPress={this.onSubmit}
                 />
             </View>
         );
     }
 }
 
-<<<<<<< HEAD
 export default LoginForm;
-=======
-export default LoginForm;
->>>>>>> 174fdf3ca7b3e4b3342b7294a102a058a0f23a69
