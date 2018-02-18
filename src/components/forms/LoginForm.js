c602765ce63d76162f317b5e1225cfd48788c94f
phuwaditever <phuwaditever@gmail.com>
2018-02-18 15:14:55 +0700

    valadation email password

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -13,28 +13,35 @@ class LoginForm extends Component{
             errors: {
                 email:'',
                 password:''
-            }
+            },
+            disabled: true
         };
 
         this.onSubmit = this.onSubmit.bind(this);
-        this.checkError = this.checkError.bind(this);
+        this.checkEmail = this.checkEmail.bind(this);
         this.checkPassword = this.checkPassword.bind(this);
         //this.onChange = this.onChange.bind(this);
     }
     checkPassword(){
-        //homework
-        console.log('checkPassword',this.state.password);
-        if(this.state.password<8)
-             this.setState({errors: {password:'not ok'}})
-        else
-            this.setState({errors: {password: ''}});
+        if(this.state.password.length<8){
+            this.setState({errors: {...this.state.errors, password:'more than 8'}});
+            this.setState({disabled:true});
+        }            
+        else{
+            this.setState({errors: {...this.state.errors, password:''}});
+            this.setState({disabled:false});
+        }            
     }
-    checkError(){
-        console.log('checkEmail',this.state.email);
-        if(!Validator.isEmail(this.state.email))
-            this.setState({errors: {email:'not email' , password:''}})
-        else
-            this.setState({errors: {email:'' , password: ''}});
+    checkEmail(){
+        if(!Validator.isEmail(this.state.email)){
+            this.setState({errors: {...this.state.errors, email:'not email format'}});
+            this.setState({disabled:true});
+        }            
+        else{
+            this.setState({errors: {...this.state.errors, email:''}});  
+            this.setState({disabled:false});
+        }
+            
     }
     onSubmit(){
         // console.log('onSubmit',this.state);
@@ -64,9 +71,15 @@ class LoginForm extends Component{
                     style={{backgroundColor:'pink',marginTop:10,marginBottom:10}}
                     secureTextEntry = 'true'
                     placeholder="password"
-                    onBlur={this.checkPassword}
+                    onKeyPress={this.checkPassword}
                 />
                 <Text>{this.state.errors.password}</Text>
+                <Button
+                    disabled
+                    color="green"
+                    title="LOGIN"
+                    onPress={this.disabled}                                   
+                />
             </View>
         );
     }
